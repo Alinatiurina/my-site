dbe45486fa1cd2102bdaa2a052e892e5503e9d86
Alinatiurina <144820507+Alinatiurina@users.noreply.github.com>
2024-09-05 17:12:55 +0300

    add mobile wersion

--- script.js
+++ script.js
@@ -14,17 +14,47 @@ const swiper = new Swiper('.swiper', {
     },
     keyboard: true,
     mousewheel: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     speed: 1000,
-    //   breakpoints: {
-    //     320: {
-    //       slidesPerView: 1,
-    //     },
-    //     768: {
-    //       slidesPerView: 3,
-    //     },
-    //     1440: {
-    //       slidesPerView: 3,
-    //     },
-    //   },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 3,
+        },
+      },
 });
+
+const btn = document.querySelector(".solid-menu-btn");
+const closeBtn = document.querySelector(".mobile-close-btn");
+const mob = document.querySelector(".mobile-menu");
+const mobLi = document.querySelector('.mobile-menu-nav-list');
+const mainDoc = document.querySelector('main');
+
+
+
+function mobileMenuOpen() {
+ mob.classList.add("is-open") 
+}
+function mobileMenuClose() {
+ mob.classList.remove("is-open") 
+}
+btn.addEventListener("click", mobileMenuOpen);
+closeBtn.addEventListener("click", mobileMenuClose);
+
+mainDoc.addEventListener('click', (e) => {
+
+    if (e.target !== mob) {
+        mob.classList.remove("is-open")
+        return;
+    }
+})
+
+mobLi.addEventListener('click', (e) => {
+        mob.classList.remove("is-open")
+        return;
+})
